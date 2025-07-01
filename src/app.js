5ed29079653e4270b77fa040883d611ce8d11273
vinay tripathi <vinaytripathi9868@gmail.com>
2025-07-01 16:09:12 +0530

    dynamic data in signup api call

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const  connectDB = require("./config/database");
-
+const User  = require("./models/user");
 const app = express();
+
+app.use(express.json());
+
+
+app.post("/signup",async (req,res)=>{
+    
+
+    const user = new User(req.body); 
+    try{
+    await  user.save();
+    res.send("user data  added successfully")
+    }
+    catch(err){
+        res.status(400).send("Error saving the user"+err.message);
+    }
+    
+})
 
 connectDB()
 .then(()=>{
